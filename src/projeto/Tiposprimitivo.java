b0d4986100fad3f43f609dc5f650088ea4f5b969
evrtonsmedeiros <cleodonmedeiros7@gmail.com>
2022-03-06 09:34:58 -0300

    site trabalho

---
+++ src/projeto/Tiposprimitivo.java
@@ -0,0 +1,15 @@
+package projeto;
+
+public class Tiposprimitivo {
+public static void main(String[] args) {
+	
+	
+	String s = "salario";
+	int num = 121;
+	float flut = 121;
+	double flota = 1111.10;
+	System.out.printf("%s %d %f %f", s, num, flut,flota);
+	
+}
+}
+ 
---
+++ src/projeto/console.java
@@ -0,0 +1,16 @@
+package projeto;
+
+import java.util.Scanner;
+
+public class console {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+      
+      
+      System.out.print("digite seu nome");
+	String nome = entrada.nextLine(); 
+	
+	System.out.printf("%s.",nome);
+	}
+
+}
